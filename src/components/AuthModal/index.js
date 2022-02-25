55f248d44caf6dd63561be3826a7f26ed866f8a8
Pernot-Julien <roulio555@gmail.com>
2022-02-25 11:15:31 +0100

    all controlled fields ok

--- src/components/AuthModal/index.js
+++ src/components/AuthModal/index.js
@@ -1,11 +1,22 @@
 // import dependencies
 import { useSelector, useDispatch } from 'react-redux';
-import { setNewLastName, setNewFirstname } from '../../actions';
+import { setNewLastName, setNewFirstname, setNewAddress, setNewBirthdate, setNewCity, setNewZipCode, setNewCountry, setNewPhone, setNewIban, setNewBic, setNewUserName, setNewPassword, setNewEmail } from '../../actions';
 // import styles
 
 const AuthModal = () => {
+  const email = useSelector((state) => state.email);
+  const password = useSelector((state) => state.password);
+  const username = useSelector((state) => state.username);
+  const birthDate = useSelector((state) => state.birthDate);
   const lastname = useSelector((state) => state.lastname);
   const firstname = useSelector((state) => state.firstname);
+  const address = useSelector((state) => state.address);
+  const city = useSelector((state) => state.city);
+  const zipCode = useSelector((state) => state.zipCode);
+  const country = useSelector((state) => state.country);
+  const phone = useSelector((state) => state.phone);
+  const iban = useSelector((state) => state.iban);
+  const bic = useSelector((state) => state.bic);
   console.log(lastname);
   console.log(firstname);
   const dispatch = useDispatch();
@@ -20,23 +31,55 @@ const AuthModal = () => {
           </svg>
         </button>
         <div className="inline-flex justify-center rounded-md items-center w-full text-center text-white h-12 bg-slate-500 uppercase mt-6 ">Inscription</div>
+
         <div className="w-full flex align-middle justify-center bg-gray-200 p-2 rounded-md">
           <input
             className="w-4/5 flex align-middle justify-center p-2 rounded-md"
-            placeholder="Nom"
+            placeholder="Email"
             type="email"
+            value={email}
             onChange={
-                    (event) => {
-                      dispatch(setNewLastName(event.target.value));
+                        (event) => {
+                          dispatch(setNewEmail(event.target.value));
+                        }
                     }
-          }
           />
         </div>
+
+        <div className="w-full flex align-middle justify-center bg-gray-200 p-2 rounded-md">
+          <input
+            className="w-4/5 flex align-middle justify-center p-2 rounded-md"
+            placeholder="Mot de passe"
+            type="password"
+            value={password}
+            onChange={
+                        (event) => {
+                          dispatch(setNewPassword(event.target.value));
+                        }
+                    }
+          />
+        </div>
+
+        <div className="w-full flex align-middle justify-center bg-gray-200 p-2 rounded-md">
+          <input
+            className="w-4/5 flex align-middle justify-center p-2 rounded-md"
+            placeholder="Nom d'utilisateur"
+            type="text"
+            value={username}
+            onChange={
+                        (event) => {
+                          dispatch(setNewUserName(event.target.value));
+                        }
+                    }
+          />
+        </div>
+
         <div className="w-full flex align-middle justify-center bg-gray-200 p-2 rounded-md">
           <input
             className="w-4/5 flex align-middle justify-center p-2 rounded-md"
             placeholder="Prénom"
             type="text"
+            value={firstname}
             onChange={
                         (event) => {
                           dispatch(setNewFirstname(event.target.value));
@@ -44,6 +87,133 @@ const AuthModal = () => {
                     }
           />
         </div>
+
+        <div className="w-full flex align-middle justify-center bg-gray-200 p-2 rounded-md">
+          <input
+            className="w-4/5 flex align-middle justify-center p-2 rounded-md"
+            placeholder="Nom"
+            type="text"
+            value={lastname}
+            onChange={
+                    (event) => {
+                      dispatch(setNewLastName(event.target.value));
+                    }
+          }
+          />
+        </div>
+
+        <div className="w-full flex align-middle justify-center bg-gray-200 p-2 rounded-md">
+          <input
+            className="w-4/5 flex align-middle justify-center p-2 rounded-md"
+            placeholder="Date de naissance"
+            type="date"
+            value={birthDate}
+            onChange={
+                    (event) => {
+                      dispatch(setNewBirthdate(event.target.value));
+                    }
+          }
+          />
+        </div>
+
+        <div className="w-full flex align-middle justify-center bg-gray-200 p-2 rounded-md">
+          <input
+            className="w-4/5 flex align-middle justify-center p-2 rounded-md"
+            placeholder="Adresse"
+            type="text"
+            value={address}
+            onChange={
+                    (event) => {
+                      dispatch(setNewAddress(event.target.value));
+                    }
+          }
+          />
+        </div>
+
+        <div className="w-full flex align-middle justify-center bg-gray-200 p-2 rounded-md">
+          <input
+            className="w-4/5 flex align-middle justify-center p-2 rounded-md"
+            placeholder="Ville"
+            type="text"
+            value={city}
+            onChange={
+                    (event) => {
+                      dispatch(setNewCity(event.target.value));
+                    }
+          }
+          />
+        </div>
+
+        <div className="w-full flex align-middle justify-center bg-gray-200 p-2 rounded-md">
+          <input
+            className="w-4/5 flex align-middle justify-center p-2 rounded-md"
+            placeholder="Code postal"
+            type="number"
+            value={zipCode}
+            onChange={
+                    (event) => {
+                      dispatch(setNewZipCode(event.target.value));
+                    }
+          }
+          />
+        </div>
+
+        <div className="w-full flex align-middle justify-center bg-gray-200 p-2 rounded-md">
+          <input
+            className="w-4/5 flex align-middle justify-center p-2 rounded-md"
+            placeholder="Pays"
+            type="text"
+            value={country}
+            onChange={
+                    (event) => {
+                      dispatch(setNewCountry(event.target.value));
+                    }
+          }
+          />
+        </div>
+
+        <div className="w-full flex align-middle justify-center bg-gray-200 p-2 rounded-md">
+          <input
+            className="w-4/5 flex align-middle justify-center p-2 rounded-md"
+            placeholder="Mobile"
+            type="number"
+            value={phone}
+            onChange={
+                    (event) => {
+                      dispatch(setNewPhone(event.target.value));
+                    }
+          }
+          />
+        </div>
+
+        <div className="w-full flex align-middle justify-center bg-gray-200 p-2 rounded-md">
+          <input
+            className="w-4/5 flex align-middle justify-center p-2 rounded-md"
+            placeholder="IBAN"
+            type="text"
+            value={iban}
+            onChange={
+                    (event) => {
+                      dispatch(setNewIban(event.target.value));
+                    }
+          }
+          />
+        </div>
+
+        <div className="w-full flex align-middle justify-center bg-gray-200 p-2 rounded-md">
+          <input
+            className="w-4/5 flex align-middle justify-center p-2 rounded-md"
+            placeholder="BIC"
+            type="text"
+            value={bic}
+            onChange={
+                    (event) => {
+                      dispatch(setNewBic(event.target.value));
+                    }
+          }
+          />
+        </div>
+
         <input
           className="cursor-pointer inline-flex justify-center rounded-md items-center w-full text-center text-slate- h-12 bg-green-200 uppercase mt-2 hover:bg-green-300"
           type="submit"
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,38 @@
-import { SET_NEW_FIRSTNAME, SET_NEW_LASTNAME } from '../actions';
+import { SET_NEW_ADDRESS, SET_NEW_BIC, SET_NEW_BIRTHDATE, SET_NEW_CITY, SET_NEW_COUNTRY, SET_NEW_EMAIL, SET_NEW_FIRSTNAME, SET_NEW_IBAN, SET_NEW_LASTNAME, SET_NEW_PASSWORD, SET_NEW_PHONE, SET_NEW_USERNAME, SET_NEW_ZIPCODE } from '../actions';
 
 const initialState = {
-  lastname: 'Dupont',
+  email: 'toto@gmail.com',
+  password: 'test',
+  username: 'toto',
   firstname: 'Lucien',
+  lastname: 'Dupont',
+  birthDate: '1990-07-14',
+  address: '10 rue du stade',
+  city: 'Paris',
+  zipCode: 75000,
+  country: 'France',
+  phone: '0635815264',
+  iban: 'stringfdfdf',
+  bic: 'FR76',
 };
 
 const reducer = (state = initialState, action = {}) => {
   switch (action.type) {
+    case SET_NEW_EMAIL:
+      return {
+        ...state,
+        email: action.email,
+      };
+    case SET_NEW_PASSWORD:
+      return {
+        ...state,
+        password: action.password,
+      };
+    case SET_NEW_USERNAME:
+      return {
+        ...state,
+        username: action.username,
+      };
     case SET_NEW_FIRSTNAME:
       return {
         ...state,
@@ -16,6 +42,46 @@ const reducer = (state = initialState, action = {}) => {
       return {
         ...state,
         lastname: action.value,
+      };
+    case SET_NEW_BIRTHDATE:
+      return {
+        ...state,
+        birthDate: action.birthDate,
+      };
+    case SET_NEW_ADDRESS:
+      return {
+        ...state,
+        address: action.address,
+      };
+    case SET_NEW_CITY:
+      return {
+        ...state,
+        city: action.city,
+      };
+    case SET_NEW_ZIPCODE:
+      return {
+        ...state,
+        zipCode: action.zipCode,
+      };
+    case SET_NEW_COUNTRY:
+      return {
+        ...state,
+        country: action.country,
+      };
+    case SET_NEW_PHONE:
+      return {
+        ...state,
+        phone: action.phone,
+      };
+    case SET_NEW_IBAN:
+      return {
+        ...state,
+        iban: action.iban,
+      };
+    case SET_NEW_BIC:
+      return {
+        ...state,
+        bic: action.bic,
       };
     default:
       return state;
