e50e1279c52e7c6b2fdf208a84d194633acccb9b
kapalexis <kap.alexis@gmail.com>
2022-02-27 14:40:43 +0100

    fixed responsive pb with main container causing x scrolling behavior

--- src/components/AuthModal/index.js
+++ src/components/AuthModal/index.js
@@ -1,6 +1,9 @@
 // import dependencies
 import { useSelector, useDispatch } from 'react-redux';
 
+// import components
+import HeaderMenu from 'src/components/HeaderMenu';
+
 // import actions
 import {
   setNewLastName,
@@ -43,207 +46,209 @@ const AuthModal = () => {
   };
 
   return (
+    <>
+      <HeaderMenu />
+      <div className="flex flex-col justify-center items-center my-8">
+        <form onSubmit={handleSubmit} className="w-[500px] flex flex-col justify-center items-center gap-2">
+          <button type="button" className="flex justify-center items-center rounded-full h-10 w-10 text-bg-slate-400 text-lg bg-slate-300 hover:bg-slate-400 p-2">
 
-    <div className="h-screen flex flex-col justify-center items-center">
-      <form onSubmit={handleSubmit} className="w-[500px] flex flex-col justify-center items-center gap-2">
-        <button type="button" className="flex justify-center items-center rounded-full h-10 w-10 text-bg-slate-400 text-lg bg-slate-300 hover:bg-slate-400 p-2">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-slate-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+            </svg>
+          </button>
+          <div className="inline-flex justify-center rounded-md items-center w-full text-center text-white h-12 bg-slate-500 uppercase mt-6 ">Inscription</div>
 
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-slate-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
-          </svg>
-        </button>
-        <div className="inline-flex justify-center rounded-md items-center w-full text-center text-white h-12 bg-slate-500 uppercase mt-6 ">Inscription</div>
-
-        <div className="w-full flex align-middle justify-center bg-gray-200 p-2 rounded-md">
-          <input
-            className="w-4/5 flex align-middle justify-center p-2 rounded-md"
-            placeholder="Email"
-            type="email"
-            value={email}
-            onChange={
+          <div className="w-full flex align-middle justify-center bg-gray-200 p-2 rounded-md">
+            <input
+              className="w-4/5 flex align-middle justify-center p-2 rounded-md"
+              placeholder="Email"
+              type="email"
+              value={email}
+              onChange={
                         (event) => {
                           dispatch(setNewEmail(event.target.value));
                         }
                     }
-          />
-        </div>
+            />
+          </div>
 
-        <div className="w-full flex align-middle justify-center bg-gray-200 p-2 rounded-md">
-          <input
-            className="w-4/5 flex align-middle justify-center p-2 rounded-md"
-            placeholder="Mot de passe"
-            type="password"
-            value={password}
-            onChange={
+          <div className="w-full flex align-middle justify-center bg-gray-200 p-2 rounded-md">
+            <input
+              className="w-4/5 flex align-middle justify-center p-2 rounded-md"
+              placeholder="Mot de passe"
+              type="password"
+              value={password}
+              onChange={
                         (event) => {
                           dispatch(setNewPassword(event.target.value));
                         }
                     }
-          />
-        </div>
+            />
+          </div>
 
-        <div className="w-full flex align-middle justify-center bg-gray-200 p-2 rounded-md">
-          <input
-            className="w-4/5 flex align-middle justify-center p-2 rounded-md"
-            placeholder="Nom d'utilisateur"
-            type="text"
-            value={username}
-            onChange={
+          <div className="w-full flex align-middle justify-center bg-gray-200 p-2 rounded-md">
+            <input
+              className="w-4/5 flex align-middle justify-center p-2 rounded-md"
+              placeholder="Nom d'utilisateur"
+              type="text"
+              value={username}
+              onChange={
                         (event) => {
                           dispatch(setNewUserName(event.target.value));
                         }
                     }
-          />
-        </div>
+            />
+          </div>
 
-        <div className="w-full flex align-middle justify-center bg-gray-200 p-2 rounded-md">
-          <input
-            className="w-4/5 flex align-middle justify-center p-2 rounded-md"
-            placeholder="Prénom"
-            type="text"
-            value={firstname}
-            onChange={
+          <div className="w-full flex align-middle justify-center bg-gray-200 p-2 rounded-md">
+            <input
+              className="w-4/5 flex align-middle justify-center p-2 rounded-md"
+              placeholder="Prénom"
+              type="text"
+              value={firstname}
+              onChange={
                         (event) => {
                           dispatch(setNewFirstname(event.target.value));
                         }
                     }
-          />
-        </div>
+            />
+          </div>
 
-        <div className="w-full flex align-middle justify-center bg-gray-200 p-2 rounded-md">
-          <input
-            className="w-4/5 flex align-middle justify-center p-2 rounded-md"
-            placeholder="Nom"
-            type="text"
-            value={lastname}
-            onChange={
+          <div className="w-full flex align-middle justify-center bg-gray-200 p-2 rounded-md">
+            <input
+              className="w-4/5 flex align-middle justify-center p-2 rounded-md"
+              placeholder="Nom"
+              type="text"
+              value={lastname}
+              onChange={
                     (event) => {
                       dispatch(setNewLastName(event.target.value));
                     }
           }
-          />
-        </div>
+            />
+          </div>
 
-        <div className="w-full flex align-middle justify-center bg-gray-200 p-2 rounded-md">
-          <input
-            className="w-4/5 flex align-middle justify-center p-2 rounded-md"
-            placeholder="Date de naissance"
-            type="date"
-            value={birthDate}
-            onChange={
+          <div className="w-full flex align-middle justify-center bg-gray-200 p-2 rounded-md">
+            <input
+              className="w-4/5 flex align-middle justify-center p-2 rounded-md"
+              placeholder="Date de naissance"
+              type="date"
+              value={birthDate}
+              onChange={
                     (event) => {
                       dispatch(setNewBirthdate(event.target.value));
                     }
           }
-          />
-        </div>
+            />
+          </div>
 
-        <div className="w-full flex align-middle justify-center bg-gray-200 p-2 rounded-md">
-          <input
-            className="w-4/5 flex align-middle justify-center p-2 rounded-md"
-            placeholder="Adresse"
-            type="text"
-            value={address}
-            onChange={
+          <div className="w-full flex align-middle justify-center bg-gray-200 p-2 rounded-md">
+            <input
+              className="w-4/5 flex align-middle justify-center p-2 rounded-md"
+              placeholder="Adresse"
+              type="text"
+              value={address}
+              onChange={
                     (event) => {
                       dispatch(setNewAddress(event.target.value));
                     }
           }
-          />
-        </div>
+            />
+          </div>
 
-        <div className="w-full flex align-middle justify-center bg-gray-200 p-2 rounded-md">
-          <input
-            className="w-4/5 flex align-middle justify-center p-2 rounded-md"
-            placeholder="Ville"
-            type="text"
-            value={city}
-            onChange={
+          <div className="w-full flex align-middle justify-center bg-gray-200 p-2 rounded-md">
+            <input
+              className="w-4/5 flex align-middle justify-center p-2 rounded-md"
+              placeholder="Ville"
+              type="text"
+              value={city}
+              onChange={
                     (event) => {
                       dispatch(setNewCity(event.target.value));
                     }
           }
-          />
-        </div>
+            />
+          </div>
 
-        <div className="w-full flex align-middle justify-center bg-gray-200 p-2 rounded-md">
-          <input
-            className="w-4/5 flex align-middle justify-center p-2 rounded-md"
-            placeholder="Code postal"
-            type="number"
-            value={zipCode}
-            onChange={
+          <div className="w-full flex align-middle justify-center bg-gray-200 p-2 rounded-md">
+            <input
+              className="w-4/5 flex align-middle justify-center p-2 rounded-md"
+              placeholder="Code postal"
+              type="number"
+              value={zipCode}
+              onChange={
                     (event) => {
                       dispatch(setNewZipCode(event.target.value));
                     }
           }
-          />
-        </div>
+            />
+          </div>
 
-        <div className="w-full flex align-middle justify-center bg-gray-200 p-2 rounded-md">
-          <input
-            className="w-4/5 flex align-middle justify-center p-2 rounded-md"
-            placeholder="Pays"
-            type="text"
-            value={country}
-            onChange={
+          <div className="w-full flex align-middle justify-center bg-gray-200 p-2 rounded-md">
+            <input
+              className="w-4/5 flex align-middle justify-center p-2 rounded-md"
+              placeholder="Pays"
+              type="text"
+              value={country}
+              onChange={
                     (event) => {
                       dispatch(setNewCountry(event.target.value));
                     }
           }
-          />
-        </div>
+            />
+          </div>
 
-        <div className="w-full flex align-middle justify-center bg-gray-200 p-2 rounded-md">
-          <input
-            className="w-4/5 flex align-middle justify-center p-2 rounded-md"
-            placeholder="Mobile"
-            type="text"
-            value={phone}
-            onChange={
+          <div className="w-full flex align-middle justify-center bg-gray-200 p-2 rounded-md">
+            <input
+              className="w-4/5 flex align-middle justify-center p-2 rounded-md"
+              placeholder="Mobile"
+              type="text"
+              value={phone}
+              onChange={
                     (event) => {
                       dispatch(setNewPhone(event.target.value));
                     }
           }
-          />
-        </div>
+            />
+          </div>
 
-        <div className="w-full flex align-middle justify-center bg-gray-200 p-2 rounded-md">
-          <input
-            className="w-4/5 flex align-middle justify-center p-2 rounded-md"
-            placeholder="IBAN"
-            type="text"
-            value={iban}
-            onChange={
+          <div className="w-full flex align-middle justify-center bg-gray-200 p-2 rounded-md">
+            <input
+              className="w-4/5 flex align-middle justify-center p-2 rounded-md"
+              placeholder="IBAN"
+              type="text"
+              value={iban}
+              onChange={
                     (event) => {
                       dispatch(setNewIban(event.target.value));
                     }
           }
-          />
-        </div>
+            />
+          </div>
 
-        <div className="w-full flex align-middle justify-center bg-gray-200 p-2 rounded-md">
-          <input
-            className="w-4/5 flex align-middle justify-center p-2 rounded-md"
-            placeholder="BIC"
-            type="text"
-            value={bic}
-            onChange={
+          <div className="w-full flex align-middle justify-center bg-gray-200 p-2 rounded-md">
+            <input
+              className="w-4/5 flex align-middle justify-center p-2 rounded-md"
+              placeholder="BIC"
+              type="text"
+              value={bic}
+              onChange={
                     (event) => {
                       dispatch(setNewBic(event.target.value));
                     }
           }
+            />
+          </div>
+
+          <input
+            className="cursor-pointer inline-flex justify-center rounded-md items-center w-full text-center text-slate- h-12 bg-green-200 uppercase mt-2 hover:bg-green-300"
+            type="submit"
+            value="Valider"
           />
-        </div>
 
-        <input
-          className="cursor-pointer inline-flex justify-center rounded-md items-center w-full text-center text-slate- h-12 bg-green-200 uppercase mt-2 hover:bg-green-300"
-          type="submit"
-          value="Valider"
-        />
-
-      </form>
-    </div>
+        </form>
+      </div>
+    </>
   );
 };
 
